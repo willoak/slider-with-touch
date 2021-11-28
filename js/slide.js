fc25898d20afd45bd570d5a8825c47c757f33053
Willian Carvalho <Willss_oak@hotmail.com>
2021-11-28 13:13:05 -0300

    adicionando eventos de cliques mouseover,mouseup e mousedown

---
+++ js/slide.js
@@ -0,0 +1,37 @@
+export default class Slide {
+    constructor(slide, wrapper) {
+        this.slide = document.querySelector(slide);
+        this.wrapper = document.querySelector(wrapper);
+    }
+    onStart(event) {
+        event.preventDefault();
+        this.wrapper.addEventListener("mousemove", this.onMove);
+    }
+
+    onMove(event) {
+        console.log("moveu");
+    }
+
+    onEnd(event) {
+        console.log("acabou");
+        this.wrapper.removeEventListener("mousemove", this.onMove);
+    }
+
+    //ativa os eventos de slide
+    addSlideEvent() {
+        this.wrapper.addEventListener("mousedown", this.onStart);
+        this.wrapper.addEventListener("mouseup", this.onEnd);
+    }
+
+    bindEvents() {
+        this.onStart = this.onStart.bind(this);
+        this.onMove = this.onMove.bind(this);
+        this.onEnd = this.onEnd.bind(this);
+    }
+
+    init() {
+        this.bindEvents();
+        this.addSlideEvent();
+        return this;
+    }
+}
